a075ab35c4c2310beb55e60c025543a16c930bd8
knopa <Rebzya-gyk@yandex.ru>
2016-04-08 16:06:21 +0300

    change lab1

--- js/lab1/js/script.js
+++ js/lab1/js/script.js
@@ -5,7 +5,7 @@ var decBase = base - Math.floor(base);
 
 if ((base < 0) || (decBase != 0)) {
   alert('Некорректная основа экспоненты, введите целую, большую или равную 0')
-  var base = prompt('Основа экспоненты');
+  base = prompt('Основа экспоненты');
 }
 
 var exp = prompt('Показатель экспоненты');
@@ -13,7 +13,7 @@ var decExp = exp - Math.floor(exp);
 
 if ((exp < 0) || (decExp != 0)) {
   alert('Некорректная степень, введите целую, большую или равную 0');
-  var exp = prompt('Показатель экспоненты');
+  exp = prompt('Показатель экспоненты');
   result = pow(base, exp);
   console.log('Результат = ' + result);
 } else {
