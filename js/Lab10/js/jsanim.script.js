36ac3c3292d7493b7af29c01cd3136c49cbb7e48
knopa <Rebzya-gyk@yandex.ru>
2016-05-09 15:05:09 +0300

    change lab 10

--- js/Lab10/js/jsanim.script.js
+++ js/Lab10/js/jsanim.script.js
@@ -1,17 +1,44 @@
-window.onload = function() {
-    var sub = document.querySelectorAll('.sub');
-    console.log(sub);
-    var subMenu = document.querySelectorAll('.sub-menu');
+function menu() {
+    var sub = document.querySelector('.sub');
+    sub.addEventListener('mouseenter', openMenu);
+    sub.addEventListener('mouseleave', closeMenu);
 
-    for (var i=0; i<sub.length; i++) {
-        console.log(sub[i]);
-        sub[i].addEventListener('mouseenter', function() {
-            console.log(this);
-            sub[i].firstChild.style.display='block';
-        })
-        sub[i].addEventListener('mouseleave', function() {
-            sub[i].firstChild.style.display='none';
-        })
-    }
+    function openMenu() {
+        var subMenu = this.querySelector('.sub-menu');
+        if (subMenu) {
+            subMenu.style.display = 'block';
+            var nextSub = this.querySelector('.sub');
+            if (nextSub) {
+                nextSub.addEventListener('mouseenter', openMenu);
+            }
+        } else {
+            return;
+        }
+    };
 
+    function closeMenu() {
+        var subMenu = this.querySelector('.sub-menu');
+        if (subMenu) {
+            var fps = 50;
+
+            var timer = setInterval(function() {
+                            if (!subMenu.style.opacity) {
+                                subMenu.style.opacity = 1;
+                            }
+                            subMenu.style.opacity = subMenu.style.opacity - 0.05;
+                            if (subMenu.style.opacity <= 0) {
+                                clearInterval(timer);
+                                subMenu.style.opacity = 1;
+                                subMenu.style.display = 'none';
+                            }
+                        }, 1000 / fps)
+
+            var nextSub = this.querySelector('.sub');
+            if (nextSub) {
+                nextSub.addEventListener('mouseleave', closeMenu);
+            }
+        }
+    };
 }
+
+document.addEventListener('DOMContentLoaded', menu);
