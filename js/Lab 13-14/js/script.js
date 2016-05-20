2e9dfeeb91164b1b42dc07000243bdc942048e15
knopa <Rebzya-gyk@yandex.ru>
2016-05-20 09:28:14 +0300

    change script

--- js/Lab 13-14/js/script.js
+++ js/Lab 13-14/js/script.js
@@ -7,21 +7,23 @@ $(function() {
             title: 'Вопрос №1',
             checkboxname: ['one','two','three'],
             id: ['1','2','3'],
-            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3']
+            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3'],
+            correct: 1
             },
             {
             title: 'Вопрос №2',
             checkboxname: ['one','two','three'],
             id: ['1','2','3'],
-            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3']
+            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3'],
+            correct: 2
             },
             {
             title: 'Вопрос №3',
             checkboxname: ['one','two','three'],
             id: ['1','2','3'],
-            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3']
+            answers:['Вариант ответа №1','Вариант ответа №2','Вариант ответа №3'],
+            correct: 3
         }],
-        correct: [1, 2, 3],
         submit: 'Проверить мои результаты'
     };
 
@@ -33,40 +35,35 @@ $(function() {
     var $content = tmpl($html, $data);
     $('.wrapper').append($content);
 
-    function testResult() {
-        var result = 0;
-        var answer = $('input:checked');
-        var correct = $data.correct;
-        for (var i = 0; i<answer.length; i++) {
-            if ($(answer[i]).attr('id') == correct[i]) {
-                result += 1;
-            }
-        }
-        return result;
-    }
-
     function showModal(e) {
         e.preventDefault();
+
         var $modal = $('<div class="modal"></div>');
-        var result = 0;
-        var answer = $('input:checked');
-        var correct = $data.correct;
-        for (var i = 0; i<answer.length; i++) {
-            if ($(answer[i]).attr('id') == correct[i]) {
-                result += 1;
+        var $result = 0;
+        var $answer = $('input:checked');
+        var $correct = [];
+
+        for (var i = 0; i<$data.questions.length; i++) {
+                $correct[i] = $data.questions[i].correct;
+            if ($($answer[i]).attr('id') == $correct[i]) {
+                $result += 1;
                 $modal.append('<p class="correct">Ответ на ' + (i+1) + ' вопрос <b>правильный</b></p>');
             } else {
                 $modal.append('<p class="incorrect">Ответ на ' + (i+1) + ' вопрос <b>неправильный</b></p>');
             }
         }
-        $modal.append('<h4 class="result">Всего правильных ответов: ' + result + '</h4><hr>');
-        if (result == $data.questions.length) {
+
+        $modal.append('<h4 class="result">Всего правильных ответов: ' + $result + '</h4><hr>');
+
+        if ($result == $data.questions.length) {
             $modal.append('<h4>Поздравляем! Вы успешно прошли тест</h4>');
         } else {
             $modal.append('<h4>Tест не пройден</h4>');
         }
+
         $modal.append('<button>Закрыть и начать заново</button>');
         $('body').append($modal);
+
         $('button').one('click', function (e) {
             e.preventDefault();
             $modal.detach();
