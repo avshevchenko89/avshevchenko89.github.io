f5709c1f31c5cb420d9cbf5ffb0cd31bb30a15a8
knopa <shevchenko.zabolotna@gmail.com>
2016-05-28 16:27:27 +0300

    change method

--- js/Lab 15-16/js/script.js
+++ js/Lab 15-16/js/script.js
@@ -43,34 +43,35 @@ $(function () {
         this.weight = weight;
     }
 
-    Worker.prototype = new Human;
-    Student.prototype = new Human;
+    Worker.prototype = Object.create(Human.prototype);
+    Worker.prototype.constructor = Worker;
+    Student.prototype = Object.create(Human.prototype);
+    Student.prototype.constructor = Student;
 
+    Human.prototype.does = function() {
+        console.log('Занятие: ' + this.work);
+    }
     function Worker(name, age, gender, height, weight, workplace, sale) {
         Human.apply(this, arguments);
         this.workplace = workplace;
         this.sale = sale;
-        this.work = function () {
-            console.log('Я работаю!');
-        }
+        this.work = 'Я работаю!';
     }
 
     function Student(name, age, gender, height, weight, study, sale) {
         Human.apply(this, arguments);
         this.study = study;
         this.sale = sale;
-        this.does = function () {
-            console.log('Смотрю сериалы');
-        }
+        this.work = 'Смотрю сериалы';
     }
 
     var vasya = new Worker('Vasilii', 28, 'male', 190, 90, 'factory', 3000);
     console.log(vasya);
-    vasya.work();
+    vasya.does();
 
     var tanya = new Worker('Tatyana', 33, 'female', 175, 60, 'plant', 5000);
     console.log(tanya);
-    tanya.work();
+    tanya.does();
 
     var olya = new Student('Olga', 20, 'female', 170, 55, 'university', 500);
     console.log(olya);
