028f0af5f02c9ab7bd2e22368fa05dd451cbd663
knopa <shevchenko.zabolotna@gmail.com>
2016-06-01 15:58:00 +0300

    try to do livereload

--- js/Lab 17-18/gulp/gulpfile.js
+++ js/Lab 17-18/gulp/gulpfile.js
@@ -8,25 +8,32 @@ var uglify = require('gulp-uglify');
 var server = require('gulp-server-livereload');
 var watch = require('gulp-watch');
 var debug = require('gulp-debug');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
     return gulp.src('src/css/*.css')
+        .pipe(sourcemaps.init())
         .pipe(concatCss('style.min.css'))
         .pipe(gulp.dest('dist'))
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('scripts', function() {
     return gulp.src('src/js/*.js')
+        .pipe(sourcemaps.init())
         .pipe(concat('script.min.js'))
         .pipe(gulp.dest('dist'))
         .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('pages', function () {
     return gulp.src('src/*.html')
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
 });
 
@@ -35,7 +42,7 @@ gulp.task('webserver', function() {
         .pipe(debug({title: 'src'}))
         .pipe(server({
             livereload: true,
-            directoryListing: false,
+            directoryListing: true,
             open: true,
             defaultFile: 'index.html',
         }))
@@ -45,7 +52,7 @@ gulp.task('webserver', function() {
 gulp.task('default', function () {
 
     gulp.start('pages', 'styles', 'scripts', 'webserver');
-
+    
     gulp.watch('src/*.html', ['pages']);
     gulp.watch('src/css/*.css', ['styles']);
     gulp.watch('src/js/*.js', ['scripts']);
